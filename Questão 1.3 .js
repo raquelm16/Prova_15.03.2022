eebe5162b4aae414e8c212eca9e9a897c0a0630b
raquelm16 <100868145+raquelm16@users.noreply.github.com>
2022-03-15 13:28:41 -0300

    Create Questão 1.3 .js
    feito por Valentina Kroth

---
+++ Questão 1.3 .js
@@ -0,0 +1,19 @@
+var isAprovado = true
+if(isAprovado) {console.log("9");
+}
+
+var isReprovado = false
+if(isReprovado) {console.log("4");
+}
+
+var isAprovado = true
+if(isAprovado) {console.log("7");
+}
+
+var isAprovado = true
+if(isAprovado) {console.log("8");
+}
+
+var isReprovado = false
+if(isReprovado) {console.log("2");
+}
