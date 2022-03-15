002af4bddefed2982fa75c84eb0ebabfd20bf99b
raquelm16 <100868145+raquelm16@users.noreply.github.com>
2022-03-15 11:28:43 -0300

    Add files via upload

---
+++ Questão 1.4 .js
@@ -0,0 +1,7 @@
+var r = 2500
+var j = 5000
+var i = 3456
+var v = 1200
+var y = 500
+
+console.log("Raquel ganha " + r + ", " + "Julia ganha " + j + ", " + "Isadora ganha " + i + ", " + "Valentina ganha " + v + " e " + "Yuri ganha " + y + ".");
